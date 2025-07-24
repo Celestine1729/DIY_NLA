6a885a23cef95031df0adfcf83c9d7ab6ddad6d9
Celestine <151845714+Celestine1729@users.noreply.github.com>
2025-07-25 00:12:02 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="DIY_NLA",
-    version="0.1.0",  # Will be automated later
+    version="1.0.1",  # Will be automated later
     packages=find_packages(),
     install_requires=["numpy", "scipy"],
     python_requires=">=3.7",
